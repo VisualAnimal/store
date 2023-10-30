f3a4163e528b6a2ddbc28ae3f4c22093869d7754
unknown <visualanimal@icloud.com>
2023-10-31 01:13:25 +0800

    为每个不同的容量添加样式，方便浏览

--- frontend-react-vite/src/routes/list.jsx
+++ frontend-react-vite/src/routes/list.jsx
@@ -1,25 +1,26 @@
 import { gql, useQuery } from "@apollo/client"
 
 const GET_BRANDS = gql`
-    query Brands {
-      brands(orderBy:{orderNumber: asc}) {
-                    id name
-                    models(orderBy:{orderNumber: asc}) {
-                        id name
-                        capacities(orderBy:{orderNumber: asc}) {
-                            products{
-                                id
-                                brand{name} 
-                                model{name} 
-                                capacity{name} 
-                                color{name} 
-                                version{name} 
-                                price
-                            }
-                        }
-                    }
-                }
+  query Brands {
+    brands(orderBy:{orderNumber: asc}) {
+      id name products{id}
+      models(orderBy:{orderNumber: asc}) {
+        id name products{id}
+        capacities(orderBy:{orderNumber: asc}) {
+          products{id}
+          products{
+            id
+            brand{name} 
+            model{name} 
+            capacity{name} 
+            color{name} 
+            version{name} 
+            price
+          }
+        }
+      }
     }
+  }
 `
 
 export function DisplayBrnads() {
@@ -29,13 +30,13 @@ export function DisplayBrnads() {
   if (error) return <p>Error : {error.message}</p>;
 
   return (
-    <table border={1} align="center">
+    <table bgcolor="" border={1} align="center">
       <tbody>
         <tr>
           <th style={{ "backgroundColor": "#99ff99" }} colSpan={6}>新新二手报价</th>
         </tr>
         <tr>
-          <td style={{ "backgroundColor": "#fdf5e6" }} colSpan={6}>电话：13028809527 微信同步</td>
+          <td style={{ "backgroundColor": "" }} colSpan={6}>电话：13028809527 微信同步</td>
         </tr>
         <tr>
           <td>品牌</td>
@@ -45,22 +46,41 @@ export function DisplayBrnads() {
           <td>版本</td>
           <td>价格</td>
         </tr>
-        {data.brands.map(brand => (
-          brand.models.map(model => (
-            model.capacities.map(capacity => (
-              capacity.products.map(product => (
-                <tr key={product.id}>
-                  <td>{product.brand?.name}</td>
-                  <td>{product.model?.name}</td>
-                  <td>{product.capacity?.name}</td>
-                  <td>{product.color?.name}</td>
-                  <td>{product.version?.name}</td>
-                  {product.price ? (<td>{(parseInt(product?.price) + 100)}</td>) : null}
-                </tr>
-              ))
-            ))
-          ))
-        ))}
+        {data.brands.map(function (brand) {
+          console.log(brand);
+          if (!brand.products.length) {
+            return
+          }
+          return (
+            brand.models.map(function (model) {
+              if (!model.products.length) {
+                return
+              }
+              return (
+                model.capacities.map(function (capacity) {
+                  if (!capacity.products.length) {
+                    return
+                  }
+                  return (
+                    <>
+                      {capacity.products.map(product => (
+                        <tr key={product.id}>
+                          <td>{product.brand?.name}</td>
+                          <td>{product.model?.name}</td>
+                          <td>{product.capacity?.name}</td>
+                          <td>{product.color?.name}</td>
+                          <td>{product.version?.name}</td>
+                          {product.price ? (<td>{(parseInt(product?.price) + 100)}</td>) : null}
+                        </tr>
+                      ))}
+                      <tr style={{ backgroundColor: "#99ff99" }}><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                    </>
+                  )
+                })
+              )
+            })
+          )
+        })}
       </tbody>
     </table>
   )
